8215025ac506968a01253a490ec394e734eeb35f
Orazio <orazioventrella@hotmail.it>
2022-02-04 18:11:17 +0100

    feat: add new table posts

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/', 'GuestController@home')->name('home');
 Route::post('/login', 'Auth\LoginController@login') -> name('login');
 Route::post('/register', 'Auth\RegisterController@register') -> name('register');
 Route::get('/logout', 'Auth\LoginController@logout') -> name('logout');
+
+Route::get('/posts', 'PostsController@index');
+Route::get('/posts/create', 'PostsController@create');
+// store?
